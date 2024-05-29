524659d7fb75ee4f95a6817d41a85c82a34a73af
Rohith <rohith.jade@gmail.com>
2024-05-29 22:57:03 +0530

    cleaning code

--- application.py
+++ application.py
@@ -40,6 +40,7 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 
-# if __name__=="__main__":
-#     application.run(host="0.0.0.0")       
+if __name__=="__main__":
+    application.run()
+    # application.run(host="0.0.0.0")        
 
